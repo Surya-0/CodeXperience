f2aabceb1a79047667da19af1140cc263c7705e1
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-04-02 22:11:39 +0530

    Leetcode begins

---
+++ Arrays_and_Hashing/Contains_Duplicates.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        s = set()
+        # d = {}
+        for i in nums:
+            if i in s:
+                return True
+            s.add(i)
+        return False
