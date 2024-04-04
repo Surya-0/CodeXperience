3ec74fd7983e3b716c58de188d7592e4b44a36ea
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-04-05 00:10:12 +0530

    Leetcode on the run

---
+++ Arrays_and_Hashing/Group_Anagrams.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+
+def groupAnagrams(strs):
+    """
+    :type strs: List[str]
+    :rtype: List[List[str]]
+    """
+    d = defaultdict(list)
+    for string in strs:
+        count = [0] * 26
+        for c in string:
+            count[ord(c) - ord('a')] += 1
+        d[tuple(count)].append(string)
+
+    return d.values()
+
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(groupAnagrams(strs))
