85e725d0f862a0951d35aadb14b42d6f6702e199
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-04-12 23:39:53 +0530

    Leetcode on the run

---
+++ Stacks/Rev_Polish_Not.py
@@ -0,0 +1,31 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for i in tokens:
+            # print(stack)
+            if i == "+":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(a + b)
+
+            elif i == "*":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(a * b)
+                # print(stack)
+
+            elif i == "-":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(b - a)
+
+            elif i == "/":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(b / a))
+
+
+            else:
+                stack.append(int(i))
+
+        return stack.pop()
