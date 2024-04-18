6c06333c578095e9fb34ca32f456ee98e8ee9ba4
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-04-18 18:10:46 +0530

    Leetcode on the run

---
+++ Stacks/Generate_Parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generateParenthesis(self, n: int) :
+        stack = []
+        res = []
+
+        def backtrack(open_count, closed_count):
+            if open_count == closed_count == n:
+                res.append(''.join(stack))
+                return
+
+            if open_count < n:
+                stack.append('(')
+                backtrack(open_count + 1, closed_count)
+                stack.pop()
+
+            if closed_count < open_count:
+                stack.append(')')
+                backtrack(open_count, closed_count + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
+
