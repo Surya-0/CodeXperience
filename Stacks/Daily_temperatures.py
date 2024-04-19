a487e63f2f1ab37a005de6ece59b78f11fbe85e6
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-04-19 16:04:30 +0530

    Leetcode on the run

--- Stacks/Daily_temperatures.py
+++ Stacks/Daily_temperatures.py
@@ -1,9 +1,15 @@
 class Solution:
     def dailyTemperatures(self, temperatures):
-        res = [0]*len(temperatures)
-        for i in range(len(temperatures)-1):
-            for j in range(i+1,len(temperatures)):
-                if temperatures[i] < temperatures[j]:
-                    res[i] = j - i
-                    break
+
+        res = [0] * len(temperatures)
+        stack = []
+
+        for i, t in enumerate(temperatures):
+
+            while stack and stack[-1][0] < t:
+                stackval, stackind = stack.pop()
+                res[stackind] = (i - stackind)
+
+            stack.append([t, i])
+
         return res
