a893b48b5425efff08bbcbc126419d58e29f9222
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-04-28 13:27:10 +0530

    Leetcode on the run

---
+++ Sliding_Window_protocol/Buying_Selling_Stocks.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices):
+        l = 0
+        r = 1
+        maxprofit = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+
+                profit = prices[r] - prices[l]
+
+                maxprofit = max(profit, maxprofit)
+
+            else:
+                l = r
+
+            r += 1
+
+        return maxprofit
