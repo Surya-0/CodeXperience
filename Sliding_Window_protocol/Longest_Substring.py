3a8f1dfc868149682221edbc47a9628232714b58
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-04-28 16:06:26 +0530

    Leetcode on the run

---
+++ Sliding_Window_protocol/Longest_Substring.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        myset = set()
+        res = 0
+        for r in range(len(s)):
+
+            while s[r] in myset:
+                myset.remove(s[l])
+                l += 1
+
+            myset.add(s[r])
+            res = max(res, r - l + 1)
+
+        return res
+
+
+
+
+
