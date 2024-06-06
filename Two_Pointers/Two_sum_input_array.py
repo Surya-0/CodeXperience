7fd314db39310f647f836e8c76eb9a358183919e
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-06-06 23:44:47 +0530

    Leetcode on the run

--- Two_Pointers/Two_sum_input_array.py
+++ Two_Pointers/Two_sum_input_array.py
@@ -1,19 +1,11 @@
-class Solution(object):
-    def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        l = 0
-        r = len(numbers) - 1
-        while l <= r:
-            if numbers[l] + numbers[r] == target:
-                return [l + 1, r + 1]
+class Solution:
+    def twoSum(self, nums: 'List[int]', target: int) -> 'List[int]':
+        d = {}
+        for i in range(0, len(nums)):
+            rem = target - nums[i]
+            if rem in d:
+                return [i, d[rem]]
+            d[nums[i]] = i
 
-            elif numbers[l] + numbers[r] > target:
-                r = r - 1
 
-            else:
-                l = l + 1
 
--- Two_Pointers/Valid_Palindrome.py
+++ Two_Pointers/Valid_Palindrome.py
@@ -1,26 +1,20 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        string = ""
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ls = []
         for char in s:
             if char.isalnum():
                 if char.isalpha():
                     char = char.lower()
-                string+=char
-        print(string)
-        l = 0
-        r = len(string) - 1
-        if string == "":
-            return True
-        else:
-            while l<= r:
-                if string[l] == string[r]:
-                    l+=1
-                    r-=1
-                else:
-                    return False
-            return True
+                ls.append(char)
+        print(ls)
 
+        l = 0
+        r = len(ls) - 1
+
+        while l <= r:
+            if ls[l] != ls[r]:
+                return False
+            l = l + 1
+            r = r - 1
+
+        return True
