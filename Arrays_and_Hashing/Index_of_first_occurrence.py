359dfe758d45e737612d2e22934a7ab562331a6c
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-06-08 18:50:38 +0530

    Leetcode on the run

--- Arrays_and_Hashing/Index_of_first_occurrence.py
+++ Arrays_and_Hashing/Index_of_first_occurrence.py
@@ -1,8 +1,6 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if not needle:
-            return 0
-
-        substr_map = {haystack[i:i + len(needle)]: i for i in range(len(haystack) - len(needle) + 1)}
-
-        return substr_map.get(needle, -1)
+        for i in range(0,len(haystack)- len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
---
+++ Trees/Traversals.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+f = Node(6)
+g = Node(7)
+
