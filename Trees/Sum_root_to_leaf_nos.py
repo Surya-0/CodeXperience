1e0a9e720d289ec47a4c97343c9e93bb5180106d
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-06-10 01:15:08 +0530

    Leetcode on the run

---
+++ Trees/Sum_root_to_leaf_nos.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: 'Optional[TreeNode]') -> int:
+        arr_sum = [0]
+
+        def dfs(root, s):
+            if root is None:
+                return
+
+            s += str(root.val)
+
+            if root.left is None and root.right is None:
+                arr_sum[0] += int(s)
+
+            dfs(root.left, s)
+            dfs(root.right, s)
+
+            return
+
+        s = ""
+        dfs(root, s)
+        return arr_sum[0]
