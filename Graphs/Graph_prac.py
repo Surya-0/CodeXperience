9c3c18b9d7b7ef696dc45847e563b4758cb8401f
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-06-15 09:28:52 +0530

    Leetcode on the run

--- Graphs/Graph_prac.py
+++ Graphs/Graph_prac.py
@@ -3,6 +3,8 @@ class Graphs:
 
     def __init__(self):
         self.adj = defaultdict(list)
+        self.visited = set()
+        self.res_arr_dfs = []
 
     def add_edge(self,vertex1,vertex2):
         self.adj[vertex1].append(vertex2)
@@ -11,6 +13,16 @@ class Graphs:
     def print_adj_list(self):
         print(self.adj)
 
+    def dfs(self,node):
+        if node in self.visited:
+            # print("Since ",node," is in visited we are backtracking")
+            return
+
+        self.visited.add(node)
+        self.res_arr_dfs.append(node)
+        neighbours = self.adj[node]
+        for neighbor in neighbours:
+            self.dfs(neighbor)
 
 g = Graphs()
 g.add_edge(0,1)
@@ -28,4 +40,5 @@ g.add_edge(5,3)
 g.add_edge(3,2)
 g.add_edge(3,4)
 g.print_adj_list()
-
+g.dfs(0)
+print(g.res_arr_dfs)
