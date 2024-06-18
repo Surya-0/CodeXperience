b349129bfaaf7596fef35b78691b968010edd382
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-06-19 01:10:51 +0530

    Leetcode on the run

--- Two_Pointers/trial.py
+++ Two_Pointers/trial.py
@@ -1,17 +1,21 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        new = ""
-        for i in s:
-            if i.isalnum():
-                new += i.lower()
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         new = ""
+#         for i in s:
+#             if i.isalnum():
+#                 new += i.lower()
+#
+#         l = 0
+#         r = len(new) - 1
+#         while l <= r:
+#             if new[l] == new[r]:
+#                 l += 1
+#                 r -= 1
+#             else:
+#                 return False
+#
+#         return True
 
-        l = 0
-        r = len(new) - 1
-        while l <= r:
-            if new[l] == new[r]:
-                l += 1
-                r -= 1
-            else:
-                return False
-
-        return True
+l = [11,12,13,14,15,16,17,18,19,20]
+for i,val in enumerate(l):
+    print(i," ",val)
