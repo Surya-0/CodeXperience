e30cb0da89047203fd6d13b1ced4674f3375b422
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-06-22 18:38:43 +0530

    Leetcode on the run

--- Bit_Manipulation/Missing_Number.py
+++ Bit_Manipulation/Missing_Number.py
@@ -9,3 +9,16 @@ class Solution:
             res = res ^ i
 
         return res
+
+
+class Solution2:
+    def missingNumber(self, nums: 'List[int]') -> int:
+        n = len(nums)
+        s = n * (n + 1) / 2
+
+        arr_s = 0
+        for i in nums:
+            arr_s += i
+
+        return int(s - arr_s)
+
