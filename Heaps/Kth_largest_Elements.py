d565e05547f69e5e844aa3d32deb71d844904cb9
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-06-25 12:25:21 +0530

    Leetcode on the run

---
+++ Heaps/Kth_largest_Elements.py
@@ -0,0 +1,14 @@
+# class Solution:
+#     def findKthLargest(self, nums: 'List[int]', k: int) -> int:
+#         nums.sort()
+#
+#         return nums[-k]
+
+import heapq
+class Solution:
+    def findKthLargest(self, nums: 'List[int]', k: int) -> int:
+        heapq.heapify(nums)
+        while len(nums) > k:
+            heapq.heappop(nums)
+
+        return nums[0]
