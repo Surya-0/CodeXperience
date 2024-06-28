e43d9b40103ee6ba96f7454698d2c7b6393ba885
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-06-28 19:09:33 +0530

    Leetcode 75

---
+++ LeetCode-75/Queue/Dota2Senate.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        senate = list(senate)
+        D, R = deque(), deque()
+
+        for i, c in enumerate(senate):
+            if c == "R":
+                R.append(i)
+
+            else:
+                D.append(i)
+
+        while D and R:
+            dTurn = D.popleft()
+            rTurn = R.popleft()
+
+            if rTurn < dTurn:
+                R.append(rTurn + len(senate))
+
+            else:
+                D.append(dTurn + len(senate))
+
+        return "Radiant" if R else "Dire"
