0473770c3214664edab39c6569815ffb977febb1
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-07-01 08:58:33 +0530

    Leetcode 75

--- LeetCode-75/Heaps/Max_Subsequence_Sums.py
+++ LeetCode-75/Heaps/Max_Subsequence_Sums.py
@@ -1,20 +1,21 @@
 import heapq
 class Solution:
-    def kSmallestPairs(self, nums1: 'List[int]', nums2: 'List[int]', k: int) -> 'List[List[int]]':
-        if not nums1 or not nums2 or k == 0:
-            return []
+    def maxScore(self, nums1: 'List[int]', nums2: 'List[int]', k: int) -> int:
+        pairs = [(n1, n2) for n1, n2 in zip(nums1, nums2)]
+        pairs = sorted(pairs, key=lambda p: p[1], reverse=True)
 
-        min_heap = []
-        # Initialize the heap with the smallest element from each row
-        for i in range(min(k, len(nums1))):
-            heapq.heappush(min_heap, (nums1[i] + nums2[0], i, 0))
-        # print(min_heap)
-        res = []
-        # Extract the smallest pairs up to k times
-        while min_heap and len(res) < k:
-            total, i, j = heapq.heappop(min_heap)
-            res.append([nums1[i], nums2[j]])
-            if j + 1 < len(nums2):
-                heapq.heappush(min_heap, (nums1[i] + nums2[j + 1], i, j + 1))
+        minheap = []
+        n1Sum = 0
+        res = 0
+        for n1, n2 in pairs:
+            n1Sum += n1
+            heapq.heappush(minheap, n1)
+
+            if len(minheap) > k:
+                n1Pop = heapq.heappop(minheap)
+                n1Sum -= n1Pop
+
+            if len(minheap) == k:
+                res = max(res, n1Sum * n2)
 
         return res
