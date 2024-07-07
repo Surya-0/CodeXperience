20e8315e7c5b1792e3c49cf8928af8dc3a7104dd
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-07-07 18:33:00 +0530

    Leetcode 75

---
+++ Dynamic_programming/Tribonacci_number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n < 3:
+            if n == 0:
+                return 0
+
+            elif n == 1:
+                return 1
+
+            else:
+                return 1
+
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 2] + dp[i - 1] + dp[i - 3]
+
+        return dp[n]
