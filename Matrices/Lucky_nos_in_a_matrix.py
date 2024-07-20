3d00f8bb53b0315d8a9653515301c93f476edef1
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-07-20 08:57:13 +0530

    LeetCode 150

---
+++ Matrices/Lucky_nos_in_a_matrix.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
+        row_set = set()
+        col_set = set()
+        m = len(matrix)
+        n = len(matrix[0])
+        print(matrix, " ", len(matrix))
+        for i in range(m):
+            row_set.add(min(matrix[i]))
+
+        for i in range(n):
+            col_set.add(max([row[i] for row in matrix]))
+
+        return list(row_set.intersection(col_set))
