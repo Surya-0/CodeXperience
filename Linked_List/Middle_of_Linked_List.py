d7cffde7373977ce65beb7a6207fe42be73ee5ef
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-07-20 23:22:13 +0530

    NeetCode 150

---
+++ Linked_List/Middle_of_Linked_List.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow.next if fast else slow
