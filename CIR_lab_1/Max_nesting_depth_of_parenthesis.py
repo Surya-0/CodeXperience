d1953f49fada1d515e4de22bffce5190ee616ba9
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-07-22 17:52:01 +0530

    NeetCode 150

---
+++ CIR_lab_1/Max_nesting_depth_of_parenthesis.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        res = 0
+        stack = []
+        for i in s:
+            if i == "(":
+                stack.append(i)
+                res = max(res, len(stack))
+
+            elif i == ")":
+                stack.pop()
+
+        return res
