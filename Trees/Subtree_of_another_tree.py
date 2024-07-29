6f7b70ef054ab54e81175d081f1dae181b521f76
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-07-29 18:16:24 +0530

    LeetCode on the run

--- Trees/Subtree_of_another_tree.py
+++ Trees/Subtree_of_another_tree.py
@@ -1,34 +1,36 @@
-o# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def isSubtree(self, root: 'Optional[TreeNode]', subRoot: 'Optional[TreeNode]') -> bool:
+
+    def isSameTree(self, root, subroot):
+        if not root and not subroot:
+            return True
+
+        if not root or not subroot:
+            return False
+
+        left = self.isSameTree(root.left, subroot.left)
+        right = self.isSameTree(root.right, subroot.right)
+
+        return root.val == subroot.val and left and right
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         res = [False]
 
-        def sameTree(root, subroot):
-            if not root and not subroot:
-                return True
-
-            if not root or not subroot:
+        def dfs(root):
+            if root is None:
                 return False
 
-            left = sameTree(root.left, subroot.left)
-            right = sameTree(root.right, subroot.right)
-            compare = left and right
-
-            return (root.val == subroot.val) and compare
-
-        def dfs(root):
-            if root is None or res[0]:
-                return
-
-                # print(root.val)
-            if sameTree(root, subRoot):
-                res[0] = True
-                return
+            if root.val == subRoot.val:
+                if self.isSameTree(root, subRoot):
+                    res[0] = True
 
             dfs(root.left)
             dfs(root.right)
@@ -36,5 +38,6 @@ class Solution:
             return
 
         dfs(root)
-
         return res[0]
+
+
