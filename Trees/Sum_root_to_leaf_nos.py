9ac99d05b1ea1f0120a373039a5e9dd9871fc6b9
Surya-0 <72782839+Surya-0@users.noreply.github.com>
2024-07-29 20:38:40 +0530

    LeetCode on the run

--- Trees/Sum_root_to_leaf_nos.py
+++ Trees/Sum_root_to_leaf_nos.py
@@ -1,11 +1,14 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def sumNumbers(self, root: 'Optional[TreeNode]') -> int:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
         arr_sum = [0]
 
         def dfs(root, s):
